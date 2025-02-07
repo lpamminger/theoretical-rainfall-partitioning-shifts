99772637cf74254e2ca15634892806cce3998302
Lucas Pamminger <lucas.pamminger@monash.edu>
2025-02-07 12:27:54 +1100

    Make function to have unique percentile changes to each parameter

--- 01-estimating-parameters-for-models.R
+++ 01-estimating-parameters-for-models.R
@@ -6,7 +6,6 @@
 
 
 
-
 # Clear environment and console ------------------------------------------------
 rm(list = ls())
 cat("\014")
@@ -83,19 +82,50 @@ summary_rainfall_stat <- rainfall_stats_per_gauge |>
 
 rainfall_control_and_multipliers <- summary_rainfall_stat |> 
   summarise(
+    # this probably should be map then expand the list 
+    # rather than copy and pasting the same function many times
     median = median(values),
-    q1 = quantile(values, 0.01, names = FALSE),
+    q5 = quantile(values, 0.05, names = FALSE), 
     q25 = quantile(values, 0.25, names = FALSE),
     q75 = quantile(values, 0.75, names = FALSE),
-    q99 = quantile(values, 0.99, names = FALSE),
+    #q80 = quantile(values, 0.8, names = FALSE),
+    q95 = quantile(values, 0.95, names = FALSE),
+    #q99 = quantile(values, 0.99, names = FALSE),
     .by = c(metric, major_climate_type)
   ) |> 
   mutate(
-    q1_multi = q1 / median,
+    q5_multi = q5 / median,
     q25_multi = q25 / median,
     q75_multi = q75 / median,
-    q99_multi = q99 / median
+    #q80_multi = q80 / median,
+    q95_multi = q95 / median#,
+    #q99_multi = q99 / median
   )
+
+
+### make_control_and_multiplier_parameters function ############################
+# example inputs:
+# metric = "Intercept"
+# small_change = "q5"
+# large_change = "q1"
+
+make_control_and_multiplier_parameters <- function(metric, small_change, large_change, data) {
+  data |> 
+    filter(metric == {{ metric }}) |> 
+    select(median, {{ small_change }}, {{ large_change }}) |>
+    rename(
+      small_change = {{ small_change }},
+      large_change = {{ large_change }}
+    ) |> 
+    mutate(
+      small_multiplier = small_change / median,
+      large_multiplier = large_change / median
+    ) |> 
+    add_column(
+      metric = {{ metric }},
+      .before = 1
+    )
+}
 
 
 ## Direction of changes for multiplier selection (from literature):
@@ -104,33 +134,26 @@ rainfall_control_and_multipliers <- summary_rainfall_stat |>
 ### - Autocorrelation = increase
 ### - Skewness = increase
 
-rainfall_temperate_parameters <- rainfall_control_and_multipliers |> 
-  filter(major_climate_type == "Temperate (C)") |> 
-  filter(metric != "Mean") |> 
-  select(metric, major_climate_type, median, q75, q99, q75_multi, q99_multi) |> 
-  rename(
-    control_parameter = median,
-    small_change_parameter = q75,
-    large_change_parameter = q99,
-    small_multiplier = q75_multi,
-    large_multiplier = q99_multi
-  )
 
-### Mannually fix the mean rainfall so its a decrease
-just_mean_rainfall <- rainfall_control_and_multipliers |> 
-  filter(major_climate_type == "Temperate (C)") |> 
-  filter(metric == "Mean") |> 
-  select(!c(q75, q99, q75_multi, q99_multi)) |> 
-  rename(
-    control_parameter = median,
-    small_change_parameter = q25,
-    large_change_parameter = q1,
-    small_multiplier = q25_multi,
-    large_multiplier = q1_multi
-  )
+temperate_rainfall_control_and_multipliers <- rainfall_control_and_multipliers |> 
+  filter(major_climate_type == "Temperate (C)")
 
-rainfall_temperate_parameters <- rbind(just_mean_rainfall, rainfall_temperate_parameters) |> 
-  # order metric
+rainfall_temperate_parameters <- pmap(
+  .l = list(
+    temperate_rainfall_control_and_multipliers |> pull(metric),
+    # Mean small q25, large q5
+    # SD small q75, large q95
+    # Autocorrelation small q75, large q95
+    # Skewness small q75, large q95
+    c("q25", "q75", "q75", "q75"), # small change
+    c("q5", "q95", "q95", "q95") # large change
+  ),
+  .f = make_control_and_multiplier_parameters,
+  data = temperate_rainfall_control_and_multipliers
+) |> 
+  list_rbind() |> 
+  rename(control = median) |> 
+  # Order facets
   mutate(
     metric = factor(
       metric, 
@@ -139,10 +162,12 @@ rainfall_temperate_parameters <- rbind(just_mean_rainfall, rainfall_temperate_pa
   )
 
 
+
 write_csv(
   rainfall_temperate_parameters,
   "./Results/rainfall_temperate_parameters.csv"
   )
+
 
 ## 3.3 Tidy summary stats ready for plotting ===================================
 ### To add a overall to boxplot copy the entire tibble replace major climate 
@@ -174,15 +199,15 @@ rainfall_boxplot <- plot_summary_rainfall_stat |>
     staplewidth = 0.25
     ) +
   geom_hline(
-    aes(yintercept = small_change_parameter), 
-    data = rainfall_temperate_parameters |> filter(major_climate_type == "Temperate (C)"),
+    aes(yintercept = small_change), 
+    data = rainfall_temperate_parameters,
     linetype = "dashed",
     colour = "#ff7f00",
     linewidth = 1
   ) +
   geom_hline(
-    aes(yintercept = large_change_parameter), 
-    data = rainfall_temperate_parameters |> filter(major_climate_type == "Temperate (C)"),
+    aes(yintercept = large_change), 
+    data = rainfall_temperate_parameters,
     linetype = "dotdash",
     colour = "#a65628",
     linewidth = 1
@@ -293,22 +318,28 @@ left_join(
 
 
 ## 4.5 Determine partitioning control and multiplier statistics ================
+### Progressively step up percentiles until we see a change:
+### - Start at 25th/75th (depending or direction we want),
+### - Step up to 80th (sd big change)
+### - Step up to 90th
+### - Step up to 95th (skew big change)
+### - Step up to 99th (auto-big change can't be cause skewness exceeds limit)
+
+
 
 partioning_control_and_multipliers <- summary_partitioning |> 
   summarise(
+    # this probably should be map then expand the list 
+    # rather than copy and pasting the same function many times
     median = median(values),
-    q1 = quantile(values, 0.01, names = FALSE),
+    q5 = quantile(values, 0.05, names = FALSE), 
     q25 = quantile(values, 0.25, names = FALSE),
     q75 = quantile(values, 0.75, names = FALSE),
+    q80 = quantile(values, 0.8, names = FALSE),
+    q95 = quantile(values, 0.95, names = FALSE),
     q99 = quantile(values, 0.99, names = FALSE),
     .by = c(metric, major_climate_type)
-  ) |> 
-  mutate(
-    q1_multi = q1 / median,
-    q25_multi = q25 / median,
-    q75_multi = q75 / median,
-    q99_multi = q99 / median
-  )
+  ) 
 
 
 ## Direction of changes for multiplier selection (from literature):
@@ -318,32 +349,26 @@ partioning_control_and_multipliers <- summary_partitioning |>
 ### - Standard deviation = increase
 ### - Skewness = increase
 
-partitioning_temperate_parameters <- partioning_control_and_multipliers |> 
-  filter(major_climate_type == "Temperate (C)") |> 
-  filter(metric != "Intercept") |> 
-  select(metric, major_climate_type, median, q75, q99, q75_multi, q99_multi) |> 
-  rename(
-    control_parameter = median,
-    small_change_parameter = q75,
-    large_change_parameter = q99,
-    small_multiplier = q75_multi,
-    large_multiplier = q99_multi
-  )
 
-### Mannually fix the mean rainfall so its a decrease
-just_intercept <- partioning_control_and_multipliers |> 
-  filter(major_climate_type == "Temperate (C)") |> 
-  filter(metric == "Intercept") |> 
-  select(!c(q75, q99, q75_multi, q99_multi)) |> 
-  rename(
-    control_parameter = median,
-    small_change_parameter = q25,
-    large_change_parameter = q1,
-    small_multiplier = q25_multi,
-    large_multiplier = q1_multi
-  )
+temperate_partioning_control_and_multipliers <- partioning_control_and_multipliers |> 
+  filter(major_climate_type == "Temperate (C)")
 
-partitioning_temperate_parameters <- rbind(just_intercept, partitioning_temperate_parameters) |> 
+partitioning_temperate_parameters <- pmap(
+  .l = list(
+    temperate_partioning_control_and_multipliers |> pull(metric),
+    # Interecpt small q25
+    # Slope small q75
+    # Auto large q99
+    # Sd large q80
+    # Skew large q95 (can't do q99 due to skewness limits)
+    c("q25", "q75", "q75", "q75", "q75"), # small change
+    c("q5", "q95", "q99", "q80", "q95") # large change
+  ),
+  .f = make_control_and_multiplier_parameters,
+  data = temperate_partioning_control_and_multipliers
+) |> 
+  list_rbind() |> 
+  rename(control = median) |> 
   # Order facets
   mutate(
     metric = factor(
@@ -353,11 +378,47 @@ partitioning_temperate_parameters <- rbind(just_intercept, partitioning_temperat
   )
 
 
-
 write_csv(
   partitioning_temperate_parameters,
   "./Results/partitioning_temperate_parameters.csv"
 )
+
+### Is there a catchment with values similar to medians? #######################
+median_partitioning_parameters <- partitioning_temperate_parameters |> 
+  select(metric, control) |> 
+  mutate(
+    metric = as.character(metric)
+  )
+
+
+find_catchments_near_medians <- function(metric, metric_median, tol, data) {
+  data |> 
+    filter(metric == {{ metric }}) |> 
+    filter(near(values, as.numeric(metric_median), tol)) |> 
+    pull(gauge)
+}
+
+
+gauges_near_medians <- pmap(
+  .l = list(
+    median_partitioning_parameters |> pull(metric),
+    median_partitioning_parameters |> pull(control),
+    c(0.25, 0.01, 0.1, 0.5, 0.1) # play with tols to get smallest number catchments
+  ),
+  .f = find_catchments_near_medians,
+  data = summary_partitioning
+) |> 
+  reduce(
+    .f = base::intersect # find common elements in vectors
+  )
+
+representative_catchments <- summary_partitioning |> 
+  filter(gauge %in% gauges_near_medians) |> 
+  arrange(gauge)
+
+
+
+
 
 
 ### Again, this is to add Overall to facets
@@ -387,15 +448,15 @@ partitioning_boxplot <- plot_summary_partitioning_stat |>
     staplewidth = 0.25
     ) +
   geom_hline(
-    aes(yintercept = small_change_parameter), 
-    data = partitioning_temperate_parameters |> filter(major_climate_type == "Temperate (C)"),
+    aes(yintercept = small_change), 
+    data = partitioning_temperate_parameters,
     linetype = "dashed",
     colour = "#ff7f00",
     linewidth = 1
   ) +
   geom_hline(
-    aes(yintercept = large_change_parameter), 
-    data = partitioning_temperate_parameters |> filter(major_climate_type == "Temperate (C)"),
+    aes(yintercept = large_change), 
+    data = partitioning_temperate_parameters,
     linetype = "dotdash",
     colour = "#a65628",
     linewidth = 1
@@ -420,3 +481,4 @@ ggsave(
   height = 210,
   units = "mm"
 )
+
